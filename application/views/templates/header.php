60ad7771bd62fe41af51b47e09e13bac2b7e03e3
brunodess <brunodess@gmail.com>
2015-04-16 20:18:05 -0300

    asdasdasd

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -10,16 +10,23 @@
 	<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 	<!-- Compiled and minified JavaScript for Materialize Framework -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/js/materialize.min.js"></script>
+	<script src="js/init.js"></script>
 </head>
 <body>
-	<nav class="light-blue lighten-1" role="navigation">
+	<nav class="green darken-2" role="navigation">
 		<div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Snooker Club</a>
 			<ul class="right hide-on-med-and-down">
 				<li><a href="#">Home</a></li>
+				<li><a href="#">Contas</a></li>
+				<li><a href="#">Aluguel</a></li>
+				<li><a href="#">Moradores</a></li>
 			</ul>
 
 			<ul id="nav-mobile" class="side-nav">
 				<li><a href="#">Home</a></li>
+				<li><a href="#">Contas</a></li>
+				<li><a href="#">Aluguel</a></li>
+				<li><a href="#">Moradores</a></li>
 			</ul>
 			<a href="#" data-activates="nav-mobile" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
 		</div>
