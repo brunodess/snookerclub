7da1db4096c3ed4dcf3696c0c90a85a84fb94b67
brunodess <brunodess@gmail.com>
2015-04-16 11:10:57 -0300

    Inclusão de templates Footer e Header
    Utilizando Starter Template do Framework Materialize, criados os
    templates de header e footer.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,6 +1,26 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	<title><?php echo $title ?> - CodeIgniter 2 Tutorial</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+	<title><?php echo $title ?> - Snooker Club</title>
+	<!-- Compiled and minified CSS for Materialize Framework -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/css/materialize.min.css">
+	
+	<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<!-- Compiled and minified JavaScript for Materialize Framework -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/js/materialize.min.js"></script>
 </head>
 <body>
-	<h1>CodeIgniter 2 Tutorial</h1>
+	<nav class="light-blue lighten-1" role="navigation">
+		<div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Snooker Club</a>
+			<ul class="right hide-on-med-and-down">
+				<li><a href="#">Home</a></li>
+			</ul>
+
+			<ul id="nav-mobile" class="side-nav">
+				<li><a href="#">Home</a></li>
+			</ul>
+			<a href="#" data-activates="nav-mobile" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+		</div>
+	</nav>
